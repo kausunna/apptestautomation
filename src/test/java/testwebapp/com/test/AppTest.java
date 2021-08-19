44c90daed18a1d097e402595f329210c99a0df17
kausunna <kausunna@gmail.com>
2021-08-19 17:56:00 +0530

    updated test

--- src/test/java/testwebapp/com/test/AppTest.java
+++ src/test/java/testwebapp/com/test/AppTest.java
@@ -17,7 +17,7 @@ public class AppTest {
 		driver.get("https://kausunnadevopsproject.azurewebsites.net/webapp/");
 		String text = driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase(" Kausunna is Learning Devops "));
+		Assert.assertTrue(text.equalsIgnoreCase("Kausunna is Learning Devops"));
 		driver.close();
 
 	}
